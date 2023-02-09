0aae7c474953ec837e1fc57bf9d9dc4dd627e88f
lukasjankuss <lukasjankuss@gmail.com>
2023-02-09 11:24:39 +0000

    second commit, adding data and writing code

---
+++ code/new.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+from sklearn.svm import SVR
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from datetime import datetime
+
+# Load the dataset
+df = pd.read_csv('data/BTC-USD.csv')
+
+# Convert the date column to Unix timestamps
+df['Date'] = df['Date'].apply(lambda x: int(datetime.strptime(x, '%Y-%m-%d').timestamp()))
+
+# Prepare the data for modeling
+X = df['Date'].values.reshape(-1, 1)
+y = df['Open'].values
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Scale the features
+scaler = MinMaxScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Train the SVM model
+regressor = SVR(kernel='rbf', C=10, epsilon=0.1)
+regressor.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = regressor.predict(X_test)
+
+# Evaluate the performance of the model
+from sklearn.metrics import mean_squared_error
+mse = mean_squared_error(y_test, y_pred)
+print('Mean Squared Error:', mse)
