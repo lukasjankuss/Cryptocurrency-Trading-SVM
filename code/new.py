b7824dadc19d0111e9d4de499d01349704edfb1d
lukasjankuss <lukasjankuss@gmail.com>
2023-03-17 12:23:51 +0000

    Added MultiOutputRegressor and GridSearchCV

--- code/new.py
+++ code/new.py
@@ -1,14 +1,15 @@
 import numpy as np
 import pandas as pd
 from sklearn.svm import SVR
-from sklearn.model_selection import train_test_split
+from sklearn.multioutput import MultiOutputRegressor
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import MinMaxScaler
 from datetime import datetime
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load the dataset
-df = pd.read_csv('data/Coinmetrics.csv')
+df = pd.read_csv('data/BTC-USDv2.csv')
 df.dropna(inplace=True)
 
 # Convert the date column to Unix timestamps
@@ -16,45 +17,57 @@ df['Date'] = df['Date'].apply(lambda x: int(datetime.strptime(x, '%Y-%m-%d').tim
 
 # Prepare the data for modeling
 X = df['Date'].values.reshape(-1, 1)
-y = df['Price'].values
+y = df[['Open', 'High', 'Low', 'Close']].values
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 # Scale the features
 scaler = MinMaxScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-# Train the SVM model
-regressor = SVR(kernel='rbf', C=10, epsilon=0.1)
-regressor.fit(X_train, y_train)
+# Define the parameter grid for the GridSearchCV
+param_grid = {
+    'estimator__kernel': ['rbf', 'linear', 'poly'],
+    'estimator__C': [0.1, 1, 10, 100],
+    'estimator__epsilon': [0.01, 0.1, 1],
+}
 
-# Make predictions on the test set
-y_pred = regressor.predict(X_test)
+# Create the SVR model
+svr = SVR()
 
-# Evaluate the performance of the model
+# Create the MultiOutputRegressor with the SVR model
+regressor = MultiOutputRegressor(svr)
+
+# Create the GridSearchCV object
+grid_search = GridSearchCV(regressor, param_grid, cv=5, verbose=2, n_jobs=-1)
+
+# Fit the GridSearchCV object to the training data
+grid_search.fit(X_train, y_train)
+
+# Print the best parameters
+print("Best parameters found: ", grid_search.best_params_)
+
+# Make predictions using the best model
+best_regressor = grid_search.best_estimator_
+y_pred = best_regressor.predict(X_test)
+
+# Evaluate the performance of the best model
 from sklearn.metrics import mean_squared_error
 mse = mean_squared_error(y_test, y_pred)
 print('Mean Squared Error:', mse)
 
 # Plot the actual vs predicted values
-fig, ax = plt.subplots(figsize=(10, 6))
-sns.lineplot(data=pd.DataFrame({'Actual': y_test, 'Predicted': y_pred}), ax=ax)
-ax.set(title='Actual vs Predicted Values', xlabel='Sample', ylabel='Price')
+fig, axs = plt.subplots(2, 2, figsize=(15, 10))
+axs = axs.ravel()
 
-# Add a shaded region for the confidence interval
-ci = 1.96 * np.std(y_pred) / np.mean(y_pred)
-ax.fill_between(x=range(len(y_pred)), y1=y_pred - ci, y2=y_pred + ci, alpha=0.2)
-
-# Add a horizontal line at the mean value
-ax.axhline(np.mean(y_test), ls='--', color='gray', label='Mean')
-
-# Add a legend
-ax.legend()
+for i, label in enumerate(['Open', 'High', 'Low', 'Close']):
+    sns.lineplot(data=pd.DataFrame({'Actual': y_test[:, i], 'Predicted': y_pred[:, i]}), ax=axs[i])
+    axs[i].set(title=f'Actual vs Predicted {label} Prices', xlabel='Sample', ylabel='Price')
+    axs[i].legend()
 
 # Add a title to the figure
-fig.suptitle('Support Vector Regression on Bitcoin Price', fontsize=16)
+fig.suptitle('Support Vector Regression on Bitcoin Price Predictions', fontsize=16)
 
 plt.show()
-
