56288d677752ff96d4b8be2527168f533c9cc7be
lukasjankuss <lukasjankuss@gmail.com>
2023-03-20 23:07:30 +0000

    Added future price predictions using ensemble method

--- code/new.py
+++ code/new.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.multioutput import MultiOutputRegressor
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import MinMaxScaler
@@ -13,10 +14,10 @@ df = pd.read_csv('data/BTC-USDv2.csv')
 df.dropna(inplace=True)
 
 # Convert the date column to Unix timestamps
-df['Date'] = df['Date'].apply(lambda x: int(datetime.strptime(x, '%Y-%m-%d').timestamp()))
+df['Date'] = df['Date'].apply(lambda x: int(datetime.strptime(x, '%d/%m/%Y').timestamp()))
 
 # Prepare the data for modeling
-X = df['Date'].values.reshape(-1, 1)
+X = df[['Date', 'Volume']].values
 y = df[['Open', 'High', 'Low', 'Close']].values
 
 # Split the data into training and testing sets
@@ -27,35 +28,32 @@ scaler = MinMaxScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-# Define the parameter grid for the GridSearchCV
-param_grid = {
-    'estimator__kernel': ['rbf', 'linear', 'poly'],
-    'estimator__C': [0.1, 1, 10, 100],
-    'estimator__epsilon': [0.01, 0.1, 1],
-}
+# Create the SVR model with the best parameters found from the previous GridSearchCV
+best_svr = SVR(kernel='rbf', C=100, epsilon=0.01)
 
-# Create the SVR model
-svr = SVR()
+# Create the Random Forest Regressor model
+rf = RandomForestRegressor(n_estimators=100, random_state=0)
 
 # Create the MultiOutputRegressor with the SVR model
-regressor = MultiOutputRegressor(svr)
+svr_regressor = MultiOutputRegressor(best_svr)
 
-# Create the GridSearchCV object
-grid_search = GridSearchCV(regressor, param_grid, cv=5, verbose=2, n_jobs=-1)
+# Create the MultiOutputRegressor with the Random Forest Regressor model
+rf_regressor = MultiOutputRegressor(rf)
 
-# Fit the GridSearchCV object to the training data
-grid_search.fit(X_train, y_train)
+# Fit both models to the training data
+svr_regressor.fit(X_train, y_train)
+rf_regressor.fit(X_train, y_train)
 
-# Print the best parameters
-print("Best parameters found: ", grid_search.best_params_)
+# Make predictions using both models
+y_pred_svr = svr_regressor.predict(X_test)
+y_pred_rf = rf_regressor.predict(X_test)
 
-# Make predictions using the best model
-best_regressor = grid_search.best_estimator_
-y_pred = best_regressor.predict(X_test)
+# Combine the predictions using a simple average
+y_pred_ensemble = (y_pred_svr + y_pred_rf) / 2
 
-# Evaluate the performance of the best model
+# Evaluate the performance of the ensemble model
 from sklearn.metrics import mean_squared_error
-mse = mean_squared_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred_ensemble)
 print('Mean Squared Error:', mse)
 
 # Plot the actual vs predicted values
@@ -63,11 +61,11 @@ fig, axs = plt.subplots(2, 2, figsize=(15, 10))
 axs = axs.ravel()
 
 for i, label in enumerate(['Open', 'High', 'Low', 'Close']):
-    sns.lineplot(data=pd.DataFrame({'Actual': y_test[:, i], 'Predicted': y_pred[:, i]}), ax=axs[i])
+    sns.lineplot(data=pd.DataFrame({'Actual': y_test[:, i], 'Predicted': y_pred_ensemble[:, i]}), ax=axs[i])
     axs[i].set(title=f'Actual vs Predicted {label} Prices', xlabel='Sample', ylabel='Price')
     axs[i].legend()
 
 # Add a title to the figure
-fig.suptitle('Support Vector Regression on Bitcoin Price Predictions', fontsize=16)
+fig.suptitle('Bitcoin Price Predictions', fontsize=16)
 
 plt.show()
