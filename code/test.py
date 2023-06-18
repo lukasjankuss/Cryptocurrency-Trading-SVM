ba4b6c6f1fc8fe23871486f12943742fe94830db
lukasjankuss <lukasjankuss@gmail.com>
2023-06-18 21:32:01 +0100

    cross-validation results and kernel scores

--- code/test.py
+++ code/test.py
@@ -1,73 +1,52 @@
-# Load the necessary libraries
-import numpy as np
-import pandas as pd
-from sklearn.svm import SVR
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.multioutput import MultiOutputRegressor
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.preprocessing import MinMaxScaler
-from datetime import datetime
-import matplotlib.pyplot as plt
-import seaborn as sns
+# ...
 
-# ... rest of your code up to the model creation ...
+# Evaluate the performance of the ensemble model and individual models
+mse_ensemble = mean_squared_error(y_test, y_pred_ensemble)
+mae_ensemble = mean_absolute_error(y_test, y_pred_ensemble)
+print('Ensemble Mean Squared Error:', mse_ensemble)
+print('Ensemble Mean Absolute Error:', mae_ensemble)
 
-# Create the SVR model
-svr = SVR()
+mse_svr = mean_squared_error(y_test, y_pred_svr)
+mae_svr = mean_absolute_error(y_test, y_pred_svr)
+print('SVR Mean Squared Error:', mse_svr)
+print('SVR Mean Absolute Error:', mae_svr)
 
-# Create the Random Forest Regressor model
-rf = RandomForestRegressor(random_state=0)
+mse_rf = mean_squared_error(y_test, y_pred_rf)
+mae_rf = mean_absolute_error(y_test, y_pred_rf)
+print('Random Forest Mean Squared Error:', mse_rf)
+print('Random Forest Mean Absolute Error:', mae_rf)
 
-# Create the MultiOutputRegressor with the SVR model
-svr_regressor = MultiOutputRegressor(svr)
+# Perform price trend prediction analysis
+price_trends_pred = np.sign(y_pred_svr[:, 3] - y_pred_svr[:, 0])  # Predicted price trends (1: increase, -1: decrease)
+price_trends_actual = np.sign(y_test[:, 3] - y_test[:, 0])  # Actual price trends from the test set
 
-# Create the MultiOutputRegressor with the Random Forest Regressor model
-rf_regressor = MultiOutputRegressor(rf)
+# Calculate additional evaluation metrics for price trend prediction
+accuracy = np.mean(price_trends_pred == price_trends_actual)
+precision = np.mean(price_trends_pred[price_trends_actual == 1] == 1)
+recall = np.mean(price_trends_pred[price_trends_pred == 1] == 1)
+f1_score = 2 * (precision * recall) / (precision + recall)
 
-# Define the parameter grid for SVR
-svr_param_grid = {
-    'estimator__C': [0.1, 1, 10, 100],
-    'estimator__epsilon': [0.01, 0.1, 1],
-    'estimator__kernel': ['linear', 'rbf']
-}
+print('Price Trend Prediction Accuracy:', accuracy)
+print('Price Trend Prediction Precision:', precision)
+print('Price Trend Prediction Recall:', recall)
+print('Price Trend Prediction F1-score:', f1_score)
 
-# Define the parameter grid for Random Forest
-rf_param_grid = {
-    'estimator__n_estimators': [10, 50, 100, 200],
-    'estimator__max_depth': [None, 10, 50, 100],
-    'estimator__min_samples_split': [2, 5, 10]
-}
+# Implement and evaluate a simple trading strategy based on the predicted price trends
+df_test = pd.DataFrame(y_test, columns=['Open', 'High', 'Low', 'Close'])
+df_test['Predicted_Trend'] = price_trends_pred
+df_test['Actual_Trend'] = price_trends_actual
 
-# Create GridSearchCV objects for SVR and Random Forest
-svr_grid_search = GridSearchCV(svr_regressor, svr_param_grid, cv=5, n_jobs=-1, verbose=2)
-rf_grid_search = GridSearchCV(rf_regressor, rf_param_grid, cv=5, n_jobs=-1, verbose=2)
+# Define trading rules
+df_test['Signal'] = np.where(df_test['Predicted_Trend'] == 1, 1, -1)
+df_test['Returns'] = df_test['Signal'] * df_test['Close'].pct_change()
+df_test['Cumulative_Returns'] = (1 + df_test['Returns']).cumprod()
+df_test['Benchmark_Returns'] = df_test['Close'].pct_change().cumsum() + 1
 
-# Fit the grid search objects
-svr_grid_search.fit(X_train, y_train)
-rf_grid_search.fit(X_train, y_train)
+# Evaluate trading strategy performance
+strategy_returns = df_test['Cumulative_Returns'].iloc[-1]
+benchmark_returns = df_test['Benchmark_Returns'].iloc[-1]
 
-# Get the best parameters
-best_svr_params = svr_grid_search.best_params_
-best_rf_params = rf_grid_search.best_params_
+print('Trading Strategy Returns:', strategy_returns)
+print('Benchmark Returns:', benchmark_returns)
 
-print("Best SVR parameters: ", best_svr_params)
-print("Best Random Forest parameters: ", best_rf_params)
-
-# Create the SVR model with the best parameters
-best_svr = SVR(kernel=best_svr_params['estimator__kernel'], C=best_svr_params['estimator__C'], epsilon=best_svr_params['estimator__epsilon'])
-
-# Create the Random Forest Regressor model with the best parameters
-best_rf = RandomForestRegressor(n_estimators=best_rf_params['estimator__n_estimators'], max_depth=best_rf_params['estimator__max_depth'], min_samples_split=best_rf_params['estimator__min_samples_split'], random_state=0)
-
-# Create the MultiOutputRegressor with the best SVR model
-svr_regressor = MultiOutputRegressor(best_svr)
-
-# Create the MultiOutputRegressor with the best Random Forest Regressor model
-rf_regressor = MultiOutputRegressor(best_rf)
-
-# Fit both models to the training data
-svr_regressor.fit(X_train, y_train)
-rf_regressor.fit(X_train, y_train)
-
-# ... rest of your code ...
-
+# ...
